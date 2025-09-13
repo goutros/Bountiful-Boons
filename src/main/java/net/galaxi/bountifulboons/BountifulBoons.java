359a812273b7778ed2eef5d5e83dbbc75b30cbb3
goutros <business@goutros.com>
2025-09-13 13:25:47 +0100

    Laid out basic mod formatting

---
+++ src/main/java/net/galaxi/bountifulboons/BountifulBoons.java
@@ -0,0 +1,49 @@
+package net.galaxi.bountifulboons;
+
+import net.minecraft.world.item.*;
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+
+// The value here should match an entry in the META-INF/neoforge.mods.toml file
+@Mod(BountifulBoons.MOD_ID)
+public class BountifulBoons {
+    public static final String MOD_ID = "bountifulboons";
+
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public BountifulBoons(IEventBus modEventBus, ModContainer modContainer) {
+        modEventBus.addListener(this::commonSetup);
+
+        NeoForge.EVENT_BUS.register(this);
+
+        modEventBus.addListener(this::addCreative);
+
+        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    private void commonSetup(FMLCommonSetupEvent event) {
+    }
+
+    // Add the example block item to the building blocks tab
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
+            event.accept(Items.APPLE);
+        }
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+
+    }
+}
---
+++ src/main/java/net/galaxi/bountifulboons/ExampleModClient.java
@@ -0,0 +1,23 @@
+package net.galaxi.bountifulboons;
+
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+
+@Mod(value = BountifulBoons.MOD_ID, dist = Dist.CLIENT)
+@EventBusSubscriber(modid = BountifulBoons.MOD_ID, value = Dist.CLIENT)
+public class ExampleModClient {
+    public ExampleModClient(ModContainer container) {
+        container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
+    }
+
+    @SubscribeEvent
+    static void onClientSetup(FMLClientSetupEvent event) {
+
+    }
+}
